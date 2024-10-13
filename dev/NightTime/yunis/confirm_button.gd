ac50dfa41a8ed9e10f21de94fff96e30b16541a8
Yunis Nabiyev <35284938+WAVForm@users.noreply.github.com>
2024-10-12 23:27:34 -0700

    refactor confirm button code and petercode
    Cleaned up some of peter's code to use my confirm buttons. Reorganized peter's folder into NightTime

--- dev/NightTime/yunis/confirm_button.gd
+++ dev/NightTime/yunis/confirm_button.gd
@@ -1,21 +1,26 @@
-extends TextureButton
+extends BaseButton
 
-var confirm_delay = 5 #seconds
+var fill_color = Color.GRAY
+var confirm_delay = 2 #seconds
 var fill_progress = false
 @onready var progress_bar = $confirm_progress
 
+signal confirmed
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	progress_bar.modulate = self.get_parent().get_node("chance_background").color #make confirm bar same color as chance
+	progress_bar.modulate = fill_color
 	
 	self.button_down.connect(func():fill_progress=true)
 	self.button_up.connect(func():fill_progress=false)
 
 func _process(delta):
 	if fill_progress:
-		progress_bar.value += (progress_bar.max_value*delta)/(confirm_delay+1)
+		progress_bar.value += (progress_bar.max_value/(confirm_delay))*delta
 	elif progress_bar.value > 0:
 		if progress_bar.value > confirm_delay:
 			progress_bar.value -= (progress_bar.max_value*delta)/(confirm_delay/progress_bar.value)
 		else:
 			progress_bar.value -= (progress_bar.max_value*delta)
+	if(progress_bar.value>=progress_bar.max_value): 
+		confirmed.emit()
