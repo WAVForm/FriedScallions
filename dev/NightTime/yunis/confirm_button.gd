13f9c1c68ada0a721e13d42a7a0439e03e524e2d
Yunis Nabiyev <yunisnabiyev@gmail.com>
2024-10-17 15:59:03 -0700

    changed confirm delay
    as per justin's complaint

--- dev/NightTime/yunis/confirm_button.gd
+++ dev/NightTime/yunis/confirm_button.gd
@@ -3,7 +3,7 @@ extends BaseButton
 class_name ConfirmButton
 
 var fill_color = Color.GRAY
-var confirm_delay = 2 #seconds
+var confirm_delay = 0.25 #seconds
 var fill_progress = false
 @onready var progress_bar = $confirm_progress
 
