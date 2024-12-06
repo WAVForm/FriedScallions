b58f95945532901e855e290f1543a2af04afa5a0
Yunis Nabiyev <35284938+WAVForm@users.noreply.github.com>
2024-12-05 22:41:48 -0800

    high light :D

--- dev/DayTime/scenes/inside.gd
+++ dev/DayTime/scenes/inside.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 class_name Inside
 
+var highlight_mat = preload("res://dev/materials/highlight.tres")
+var highlighted = []
+
 @onready var floor = $floor
 @onready var trash = $trash_can
 @onready var cake = $cake_station
@@ -19,12 +22,12 @@ signal station_reached(station_name:STATIONS)
 var navigating = [false, STATIONS.NONE]
 
 func _ready():
-	floor.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.NONE))
-	trash.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.TRASH))
-	sandwich.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.SANDWICH))
-	cake.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.CAKE))
-	coffee.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.COFFEE))
-	tea.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.TEA))
+	floor.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.NONE) elif event is InputEventMouse: unhighlight_all())
+	trash.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.TRASH) elif event is InputEventMouse: highlight(trash))
+	sandwich.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.SANDWICH) elif event is InputEventMouse: highlight(sandwich))
+	cake.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.CAKE) elif event is InputEventMouse: highlight(cake))
+	coffee.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.COFFEE) elif event is InputEventMouse: highlight(coffee))
+	tea.input_event.connect(func(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int): if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1: navigate_to(event_position, STATIONS.TEA) elif event is InputEventMouse: highlight(tea))
 
 func _physics_process(delta):
 	if navigating[0]:
@@ -38,3 +41,12 @@ func _physics_process(delta):
 func navigate_to(pos: Vector3, station:STATIONS):
 		player_agent.set_target_position(pos)
 		navigating = [true, station]
+
+func highlight(object: Area3D):
+	unhighlight_all()
+	object.get_node("model").material_override = highlight_mat
+	highlighted.append(object)
+
+func unhighlight_all():
+	for object in highlighted:
+		object.get_node("model").material_override = null
